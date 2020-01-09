855feec0ed12f1fda30374f04dcaa5992bc8467e
demittsse <somrsaltt@gmail.com>
2020-01-09 15:08:16 +0900

    Seurat code

--- 10X/run10X.sh
+++ 10X/run10X.sh
@@ -1,11 +1,17 @@
+
 cellranger count --id=run_count_10X_009 \
 --fastqs=/media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/10X/HN00119076_10X/HN00119076_10X_RawData_Outs/10X_009/H72NHCCX2 \
 --sample=10X_009 \
 --transcriptome=/home/cytogenbi2/00source/refdata-cellranger-GRCh38-3.0.0
 
+##cellranger count running time
+####Martian Runtime - '3.1.0-v3.2.3'
+####Serving UI at http://cytogenbi2-B365M-DS3H:36739?auth=dqazr6nOIg4Lsms1456Tbpw_rvjsSjL9To75QbnO0TU
 
-Martian Runtime - '3.1.0-v3.2.3'
-Serving UI at http://cytogenbi2-B365M-DS3H:36739?auth=dqazr6nOIg4Lsms1456Tbpw_rvjsSjL9To75QbnO0TU
+####Running preflight checks (please wait)...
+####2020-01-08 15:22:30 [runtime] (ready)
 
-Running preflight checks (please wait)...
-2020-01-08 15:22:30 [runtime] (ready)
+####Waiting 6 seconds for UI to do final refresh.
+####Pipestance completed successfully!
+####2020-01-08 23:27:57 Shutting down.
+####Saving pipestance info to "run_count_10X_009/run_count_10X_009.mri.tgz"
