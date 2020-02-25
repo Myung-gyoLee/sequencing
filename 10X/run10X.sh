c442c72ebdbd3f568e305211678b2d15ab864e28
demittsse <somrsaltt@gmail.com>
2020-02-25 10:22:38 +0900

    Update run10X.sh

--- 10X/run10X.sh
+++ 10X/run10X.sh
@@ -4,6 +4,12 @@ cellranger count --id=run_count_10X_009 \
 --sample=10X_009 \
 --transcriptome=/home/cytogenbi2/00source/refdata-cellranger-GRCh38-3.0.0
 
+cellranger count --id=run_count_10X_009 \
+--fastqs=/home/cytogenbi2/SingleCell/SMC009/HN00119076_10X_RawData_Outs/10X_009/H72NHCCX2 \
+--sample=10X_009 \
+--transcriptome=/home/cytogenbi2/singlecell/refdata-cellranger-GRCh38-3.0.0
+
+
 ##cellranger count running time
 ####Martian Runtime - '3.1.0-v3.2.3'
 ####Serving UI at http://cytogenbi2-B365M-DS3H:36739?auth=dqazr6nOIg4Lsms1456Tbpw_rvjsSjL9To75QbnO0TU
