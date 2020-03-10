96abfa7b1094f717fbb8540bbf40a5d2a4a486d8
demittsse <somrsaltt@gmail.com>
2020-03-10 09:56:18 +0900

    Update read_column.py

--- read_column.py
+++ read_column.py
@@ -1,11 +1,11 @@
 import sys, os
 
+print("~/read_column.py [txtfile] [delimiter]")
 with open(sys.argv[1]) as f:
-	fline = f.readline().split("\t")
+        fline = f.readline().split(sys.argv[2])
 
 linenum=0
 for line in fline:
-	linenum+=1
-	print(linenum, ":", line)
-
-
+        linenum+=1
+        if '"' in line:print(linenum, ":", line.replace('"',''))
+        else:print(linenum, ":", line)
