eebdb62d8b48ea1c1311682a32b7f58ce49291fa
demittsse <somrsaltt@gmail.com>
2020-04-07 17:21:46 +0900

    Update dataframe_tcga_gtex.jl

--- Julia/dataframe_tcga_gtex.jl
+++ Julia/dataframe_tcga_gtex.jl
@@ -1,2 +1,7 @@
 exr_GTEx[1,:]
 exr_GTEx = DataFrame([[names(ex_GTEx)]; collect.(eachrow(ex_GTEx))], [:column; Symbol.(axes(ex_GTEx,2))])
+exr_GTEx[1,2:length(exr_GTEx[1,:])]
+Array(exr_GTEx[1,2:length(exr_GTEx[1,:])])
+
+###
+names!(df, [Symbol("Col$i") for i in 1:size(df,2)])
